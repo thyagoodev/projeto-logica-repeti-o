0ee0b5d3aa3fd7c20b815ca424a404885fd57e7b
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-17 13:49:49 -0300

    atualização dos arquivos html js

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -1,33 +1,41 @@
-//ESTRUTURA DE REPETIÇÃO
-const divFor = document 
+// ESTRUTURA DE REPETIÇÃO
 
-for(i = 0;i < 10; i++ ){
+const divFor = document.querySelector('#div-for');
 
-    console.log(i, 'Amor')
-    divFor.innerHTML += '$(i) - Amor <br>'
-
+for (let i = 0; i < 10; i++) {
+    console.log(i, 'Amor');
+    divFor.innerHTML += `${i} - Amor <br>`;
 }
 
-//CONTADOR/ACUMULADOR
-const imputnum = document.querySelector('#num')
-const bitNum = document.querySelector('btn-num')
-const divContAcum = document.querySelector('#div-cont-acum')
+// CONTADOR/ACUMULADOR
 
-let cont = 0
-let acum = 0.0
+const inputNum = document.querySelector('#num');
+const bitNum = document.querySelector('#btn-num');
+const divContAcum = document.querySelector('#div-cont-acum');
+
+let cont = 0;
+let acum = 0;
+
+bitNum.addEventListener('click', (evt) => {
+
+    let numDigitado = Number(inputNum.value);
+
+    cont++;
+    acum += numDigitado;
+
+    divContAcum.innerHTML = `
+        TOTAL DE NÚMEROS DIGITADOS: ${cont} <br>
+        A SOMA DOS NÚMEROS É: ${acum}
+    `;
+
+    inputNum.value = '';
 
 
-bitNum.addEventListener('click' ), (evt)=>{
-    let numDigitado = Number (inputNum.value)
+})
 
-    cont++
-    acum += numDigitado
-
-    divContAcum.innerHTML= 'TOTAL DE NÚMERO DIGITADO É: $ {cont} <br>
-    A SOMA DOS NÚMEROS É: $ {acum}'
-    
-    inputNum.value
-
-}
-
-
+//CONTROLANDO FOR
+const inputFrase = document.querySelector('#Frase')
+const inputNumRepeticao = document.querySelector
+('#num-repeticao')
+const btnFrase = document.querySelector('#btn-frase')
+const divFrase = document.querySelector('div-frase')
